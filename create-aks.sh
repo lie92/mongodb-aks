32c902bae445fa47435e0319c96a3002def4b1b8
li eric <eric.li.tech@gmail.com>
2021-04-09 16:50:23 +0200

    create cluster]

--- create-aks.sh
+++ create-aks.sh
@@ -1,3 +1,3 @@
 az group create --name myResourceGroup --location eastus
-az aks create --resource-group myResourceGroup --name myAKSCluster --node-count 2  --generate-ssh-keys
+az aks create --resource-group myResourceGroup --name myAKSCluster --node-count 4  --generate-ssh-keys
 az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
