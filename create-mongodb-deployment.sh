2568d8e56331b3859329ce95d3d031ac442c373b
Eric Li <ext.eric.li@sncf.fr>
2021-04-09 17:56:50 +0200

    deployment works, connexion works, create collection works

--- create-mongodb-deployment.sh
+++ create-mongodb-deployment.sh
@@ -1,14 +1,19 @@
 #add password in the config/samples/mongodb.com_v1_mongodbcommunity_cr.yaml
 kubectl apply -f config/samples/mongodb.com_v1_mongodbcommunity_cr.yaml
-kubectl port-forward mongodb-svc 27018:27017
-mongo 127.0.0.1:27018
 
-rs.status().members
+kubectl run  some-mongo \
+    --env="MONGO_INITDB_ROOT_USERNAME=myuser" \
+    --env="MONGO_INITDB_ROOT_PASSWORD=test" \
+    mongo -- bash
 
-#or
-kubectl run --name some-mongo \
-    -e MONGO_INITDB_ROOT_USERNAME=myuser \
-    -e MONGO_INITDB_ROOT_PASSWORD=test \
-    mongo
+kubectl describe mongodbcommunity example-mongodb | grep Uri
+mongo "mongodb://<service-object-name>.<namespace>.svc.cluster.local:27017/" --username my-user -p test
+show dbs
+use local
 
-mongo "mongodb://<service-object-name>.<namespace>.svc.cluster.local:27017/?replicaSet=<replica-set-name>"
+#https://developer.mongodb.com/quickstart/cheat-sheet/
+#https://www.educba.com/mongodb-commands/
+
+#kubectl port-forward mongodb-svc 27018:27017
+#mongo 127.0.0.1:27018
+#rs.status().members
